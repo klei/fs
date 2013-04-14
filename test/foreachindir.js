4e30738696505583326e47834f4e90d92c4a5ed9
Joakim Bengtson <joakim.bengtson@gmail.com>
2013-04-14 15:49:18 +0200

    Removing the claimed deferred support
    Because it does not work, deferred expects an array as a result

--- test/foreachindir.js
+++ test/foreachindir.js
@@ -2,7 +2,6 @@ var should = require('chai').Should(),
     join = require('path').join;
 
 var fs = require('../');
-var forEachInDir = require('deferred').promisify(fs.forEachInDir);
 
 describe('forEachInDir', function () {
   it('should call the given callback for each file in the given dir, and provide the full path for it', function (done) {
@@ -10,17 +9,7 @@ describe('forEachInDir', function () {
     var expected = [join(__dirname, 'testfiles', 'file.json'), join(__dirname, 'testfiles', 'file.md')];
     fs.forEachInDir(join(__dirname, 'testfiles'), function (err, file) {
       should.not.exist(err);
-      expected.should.contain(file);
-      if (++count === expected.length)
-        done();
-    });
-  });
-
-  it('can be used with deferred', function () {
-    var count = 0;
-    var expected = [join(__dirname, 'testfiles', 'file.json'), join(__dirname, 'testfiles', 'file.md')];
-    forEachInDir(join(__dirname, 'testfiles')).then(function (err, file) {
-      should.not.exist(err);
+      should.exist(file);
       expected.should.contain(file);
       if (++count === expected.length)
         done();
