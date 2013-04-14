5b8f40f9c14f3662a3d6154939c22ce768a552b6
Joakim Bengtson <joakim.bengtson@gmail.com>
2013-04-14 12:29:13 +0200

    Preparing for filterDir

--- test/foreachindir.js
+++ test/foreachindir.js
@@ -1,21 +1,29 @@
-var should = require('chai').Should();
+var should = require('chai').Should(),
+    join = require('path').join;
 
 var fs = require('../');
+var forEachInDir = require('deferred').promisify(fs.forEachInDir);
 
 describe('forEachInDir', function () {
   it('should call the given callback for each file in the given dir, and provide the full path for it', function (done) {
+    var count = 0;
+    var expected = [__filename, join(__dirname, 'testfiles')];
     fs.forEachInDir(__dirname, function (err, file) {
-      file.should.equal(__filename);
-      done();
+      should.not.exist(err);
+      expected.should.contain(file);
+      if (++count === expected.length)
+        done();
     });
   });
 
   it('can be used with deferred', function () {
-    var forEachInDir = require('deferred').promisify(fs.forEachInDir);
+    var count = 0;
+    var expected = [__filename, join(__dirname, 'testfiles')];
     forEachInDir(__dirname).then(function (err, file) {
       should.not.exist(err);
-      file.should.equal(__filename);
-      done();
+      expected.should.contain(file);
+      if (++count === expected.length)
+        done();
     });
   });
 });
