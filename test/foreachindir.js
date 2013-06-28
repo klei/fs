d6e15603fd9fe882a2e914e371b7a7b1e27bfa51
Joakim Bengtson <joakim.bengtson@gmail.com>
2013-06-28 17:05:05 +0200

    Adding index and files parameters to forEachInDir callback

--- test/foreachindir.js
+++ test/foreachindir.js
@@ -6,13 +6,20 @@ var fs = require('../');
 describe('forEachInDir', function () {
   it('should call the given callback for each file in the given dir, and provide the full path for it', function (done) {
     var count = 0;
-    var expected = [join(__dirname, 'testfiles', 'file.json'), join(__dirname, 'testfiles', 'file.md')];
-    fs.forEachInDir(join(__dirname, 'testfiles'), function (err, file) {
+
+    var expected = [
+      join(__dirname, 'testfiles', 'file.json'),
+      join(__dirname, 'testfiles', 'file.md')
+    ];
+
+    fs.forEachInDir(join(__dirname, 'testfiles'), function (err, file, index) {
       should.not.exist(err);
       should.exist(file);
       expected.should.contain(file);
+      index.should.equal(count);
       if (++count === expected.length)
         done();
     });
+
   });
 });
